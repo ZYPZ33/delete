e3f5c2d2874e87435bcc623a8bbbc1b0fe4271bf
ZYPZ33 <josephhyoung@btinternet.com>
2023-10-04 14:31:14 +0100

    Added cmdline options to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-install_directory='/usr/local/bin'
-executable_name='delete'
+if [ -n "$1" ]; then executable_name=$1; else executable_name='delete'
+if [ -n "$2" ]; then install_directory=$2; else install_directory='/usr/local/bin'
 
 install delete.sh ${install_directory}/${executable_name}

2049728abe1dac2ccdda33d2dfa55c65ec20c3a7
ZYPZ33 <josephhyoung@btinternet.com>
2023-10-04 14:31:40 +0100

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-if [ -n "$1" ]; then executable_name=$1; else executable_name='delete'
-if [ -n "$2" ]; then install_directory=$2; else install_directory='/usr/local/bin'
+if [ -n "$1" ]; then executable_name=$1; else executable_name='delete';fi
+if [ -n "$2" ]; then install_directory=$2; else install_directory='/usr/local/bin';fi
 
 install delete.sh ${install_directory}/${executable_name}
